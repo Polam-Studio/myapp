bb6217e112efae66e7929eb0fccb8727b85aa72b
Felipe Castro <felipecdc09@gmail.com>
2025-01-11 15:19:27 -0300

    feat: create stand by page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,70 @@
 import Image from "next/image";
+import Link from "next/link";
+import { FiArrowRightCircle } from "react-icons/fi";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <div className="absolute top-5 w-full">
+        <div className="flex h-auto w-full items-center justify-center px-4">
+          <div className="flex w-[800px] items-center justify-between rounded-full bg-white p-3 shadow-[0_0_15px_rgba(0,0,0,0.25)]">
+            <div className="flex gap-2">
+              <Image
+                alt="Logo Polam Studio"
+                src="/logo.svg"
+                width={25}
+                height={25}
+              />
+              <h1 className="hidden text-2xl font-bold text-black sm:block">
+                Polam Studio
+              </h1>
+            </div>
+            <Link
+              href={"https://www.instagram.com/polam.studio/"}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center gap-1 rounded-full bg-gradient-to-r from-[#1E5BCB] via-[#4544BD] to-[#6143BC] py-1 pl-3 pr-1 font-semibold"
+            >
+              Orçamento
+              <FiArrowRightCircle size={30} />
+            </Link>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+      <div className="flex h-screen items-center justify-center bg-[#FFFCF8] px-4">
+        <div className="flex h-auto w-[750px] flex-col items-center justify-between gap-24 rounded-xl bg-black p-10 shadow-[0_0_10px_rgba(0,0,0,0.5)] md:flex-row">
+          <div className="flex flex-col items-start gap-3">
+            <h1 className="text-2xl font-bold">Estamos em Reforma</h1>
+            <div className="flex w-full flex-col gap-1">
+              <span className="text-base leading-5">
+                Estamos aprimorando nossa página para entregar uma experiência
+                ainda mais exclusiva. Volte logo para ver o resultado.
+              </span>
+              <span className="text-sm leading-5">
+                Enquanto isso aproveite nossos conteudos no instagram!
+              </span>
+            </div>
+            <Link
+              href={"https://www.instagram.com/polam.studio/"}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center gap-2 rounded-md bg-gradient-to-r from-[#1E5BCB] via-[#4544BD] to-[#6143BC] px-4 py-2 font-semibold"
+            >
+              Ver Instagram
+              <FiArrowRightCircle size={20} />
+            </Link>
+          </div>
+          <div className="relative hidden h-[260px] min-w-[260px] rounded-xl bg-gray-500 md:block">
+            <Image
+              alt="Banner Stand By"
+              src="/banner.png"
+              layout="fill"
+              objectFit="cover"
+              className="rounded-xl"
+            />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
